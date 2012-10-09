13755de45347e7c00d81726abbebd6e197d5688f
Yuvi Masory <ymasory@gmail.com>
2012-10-09 16:24:42 -0400

    update versions

--- project/project/plugins.scala
+++ project/project/plugins.scala
@@ -1,6 +1,6 @@
 import sbt._
-object PluginDef extends Build {
+
+object Plugins extends Build {
   lazy val root = Project("plugins", file(".")) dependsOn( g8plugin )
-  lazy val g8plugin =
-    ProjectRef(uri("git://github.com/n8han/giter8#0.4.5.1"), "giter8-plugin")
+  lazy val g8plugin = ProjectRef(uri("git://github.com/n8han/giter8#0.4.5"), "giter8-plugin")
 }
